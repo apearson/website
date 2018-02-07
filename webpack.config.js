f5bfbd088e77b8b01a4c25635c3b2f6b8d814f0c
Andrew Pearson <andrew@apearson.io>
2018-02-07 15:13:02 -0600

    Changing project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+/* Libraries */
 const path = require('path');
 
+/* Webpack config */
 module.exports = {
 	entry: './src/main.ts',
+
 	module: {
 		rules: [
 			{
@@ -11,12 +14,22 @@ module.exports = {
 			}
 		]
 	},
+
 	resolve: {
 		extensions: [ '.tsx', '.ts', '.js' ]
 	},
+
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve('public', 'dist'),
 		publicPath: '/dist/'
-	}
+	},
+
+	/* Development */
+	devServer: {
+		contentBase: path.resolve('public'),
+		watchContentBase: true,
+		host: '0.0.0.0',
+		open: true,
+	},
 };
